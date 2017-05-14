d3529a66ca5681297bc39a07eab8895a1852e4d4
Erik Bates <erik.s.bates@gmail.com>
2017-05-14 15:56:45 -0500

    Finished!

---
+++ EvenAdd/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> {1,2,3,4,5,6,7,8,9,12,14 };
+            SumList(numbers);
+            Console.ReadLine();
+        }
+
+        public static void SumList(List<int> numbers)
+        {
+            int sum = 0;
+
+            for (int i=0; i<numbers.Count; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum = sum + numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
