227a26eaec96730445ed5828785a311ee0b6bca9
Erik Bates <erik.s.bates@gmail.com>
2017-05-22 19:25:19 -0500

    asdf

---
+++ RestaurantMenu/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Menu
+    {
+        MenuItem item = new MenuItem();
+        private DateTime lastUpdate;
+
+        public DateTime LastUpdate
+        {
+            get { return lastUpdate; }
+            set { lastUpdate = value; }
+        }
+    }
+
+    public class MenuItem
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        private string desc;
+
+        public string Desc
+        {
+            get { return desc; }
+            private set { desc = value; }
+        }
+
+        private string category;
+
+        public string Category
+        {
+            get { return category; }
+            private set { category = value; }
+        }
+
+        private bool newItem;
+
+        public bool NewItem
+        {
+            get { return newItem; }
+            private set { newItem = value; }
+        }
+    }
+}
---
+++ Student/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Student
+    {
+        public string name;
+        public int id;
+        public int credits;
+        private double gpa;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Credits
+        {
+            get { return credits; }
+            set { credits = value; }
+        }
+
+        public double Gpa
+        {
+            get { return gpa; }
+            private set { gpa = value; }
+        }
+    }
+
+    public class Course
+    {
+        public string courseName;
+        public int courseNum;
+        public Dictionary<string, string> roster = new Dictionary<string, string>();
+    }
+}
