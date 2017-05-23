d484f83d88967d098a604c734360c5fe94926d28
Erik Bates <erik.s.bates@gmail.com>
2017-05-22 20:23:18 -0500

    Finished studio.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    class Menu
+    {
+        private DateTime menuUpdate;
+        private List<MenuItem> food;
+
+        public DateTime MenuUpdate { get; private set; }
+        public List<MenuItem> Food { get; private set; }
+
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    class MenuItem
+    {
+        private string name;
+        private decimal price;
+        private List<string> description;
+        private Dictionary<int, string> category;
+        private readonly DateTime created;
+
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public List<string> Description { get; private set; }
+        public Dictionary<int, string> Category { get; private set; }
+        public DateTime Created { get; }
+    }
+}
